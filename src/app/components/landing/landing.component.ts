f2ef5d73b4771e0b9a02342a7ef9a5a6e611199a
Mr. IO <43411416+Matthew-Hyndman@users.noreply.github.com>
2025-05-08 14:13:11 +0100

    intergated Cat API and a few minor adjustments in html

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,29 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BlackJackHelpService } from '../../services/black-jack-help.service';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
 
   blckJackGameRoute: string = '';
+  headers = new HttpHeaders({});
+  catFact: any;
+  showCatFact = false
 
-  constructor(private blackJackHelpService: BlackJackHelpService) {}
+  constructor(
+    private blackJackHelpService: BlackJackHelpService,
+    private httpClient: HttpClient
+  ) {}
 
   ngOnInit(): void {
-
     if (this.blackJackHelpService.isHasUserAgreedToDisclaimerNull()) {
       if (this.blackJackHelpService.isHasUserAgreedToDisclaimerTrue()) {
-        this.blckJackGameRoute = "/black-jack-game";
-        return
+        this.blckJackGameRoute = "/black-jack-game";   
       }
-    } 
+    } else {
       this.blckJackGameRoute = "/black-jack-help";
-    
+    }
+  }
+
+  getCatFact(){
+    this.httpClient.get<any>(`https://catfact.ninja/fact`, {
+      headers: this.headers
+    }).subscribe(data => {
+      this.catFact = data
+    });
+
+    this.showCatFact = true;
   }
 
 }
